618743fb75f3d850f670366eb9584210209fd340
Cooper-Rampling <1029711@student.sae.edu.au>
2025-10-22 15:43:34 +1100

    Added rough Sprite and tried to add attack function

--- 25T3_GAD314/Assets/Cooper/Scripts/PatrolBehavior.cs
+++ 25T3_GAD314/Assets/Cooper/Scripts/PatrolBehavior.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class PatrolBehavior : MonoBehaviour
 {
+    //Lines that are commented out are a attempt to have patrol work within a coroutine so other animations could be played like attack
+
+
     public GameObject pointA;
     public GameObject pointB;
     private Rigidbody2D rb;
     private Animator anim;
     private Transform currentPoint;
     public float speed;
+    //private bool attack = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +21,8 @@ public class PatrolBehavior : MonoBehaviour
         anim = GetComponent<Animator>();
         currentPoint = pointB.transform;
         anim.SetBool("isRunning", true);
+        //anim.SetBool("attackNow", false);
+        //StartCoroutine(Patrol());
     }
 
     // Update is called once per frame
@@ -42,13 +49,13 @@ public class PatrolBehavior : MonoBehaviour
             flip();
             currentPoint = pointB.transform;
         }
-
+        
     }
 
     private void flip() // changes sprite direction so it faces the way it is walking
     {
         Vector3 localScale = transform.localScale;
-        localScale.x = -1;
+        localScale.x *= -1;
         transform.localScale = localScale;
     }
 
@@ -58,4 +65,38 @@ public class PatrolBehavior : MonoBehaviour
         Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
         Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
+    /*
+    bool waitCon()
+    {
+        return attack = true;
+    }
+
+    private IEnumerator Patrol()
+    {
+        
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.linearVelocity = new Vector2(speed, 0); // point b needs to be on the right
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0); // point a needs to be on the left
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            flip();
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            flip();
+            currentPoint = pointB.transform;
+        }
+        yield return new WaitWhile(attack);
+        
+    } */
+    
 }
